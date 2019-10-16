0ca34c3cef52f528b4e7d7d6d39bc2c53d6b1dc5
mirage_J <windflowerx@gmail.com>
2019-10-16 15:55:28 +0800

    add kill process action in controller

--- Controllers/ProcessController.cs
+++ Controllers/ProcessController.cs
@@ -15,48 +15,107 @@ namespace NewWebAPP201910.Controllers
         List<Process> processList;
         const string noticeName = "Notification";
         const string uidListName = "UIDList";
+        IConfigurationRoot configuration { get { return BuildConfiguration(); } }
 
-       
-        public  ProcessController()
+        public ProcessController()
         {
-            ViewData.Add(noticeName,null);
-            ViewData.Add(uidListName,null);
+            ViewData.Add(noticeName, null);
+            ViewData.Add(uidListName, null);
         }
 
         public ActionResult Index()
         {
             GetProcessList();
-            ProcessModel[] processes=new ProcessModel[processList.Count];
+            ProcessModel[] processes = new ProcessModel[processList.Count];
             for (int i = 0; i < processes.Length; i++)
             {
-                processes[i]=new ProcessModel(){ProcessName=processList[i].ProcessName,UID=processList[i].Id};
+                processes[i] = new ProcessModel() { ProcessName = processList[i].ProcessName, UID = processList[i].Id };
             }
-            ViewData[uidListName]=processes;
+            ViewData[uidListName] = processes;
             return View();
         }
         private void GetProcessList()
         {
-            var configuration=new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.Development.json").Build();
 
-            processList=new List<Process>();
-            foreach(var process in Process.GetProcessesByName(configuration["ProcessInfo:ProcessName"]))
+            processList = new List<Process>();
+            foreach (var process in Process.GetProcessesByName(configuration["ProcessInfo:ProcessName"]))
             {
                 processList.Add(process);
             }
-            
+
         }
 
-        public ActionResult LaunchProcess(){
-            Process p=new Process();
-            p.StartInfo.FileName="iexplore";
-            if( p.Start())
-                ViewData[noticeName] = $"Process {p.ProcessName} has been started,uid as {p.Id}";
-            else
-                ViewData[noticeName]=$"Fail to launch process";
-            return View("Index");
+        private static IConfigurationRoot BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.Development.json").Build();
+        }
 
+        public ActionResult LaunchProcessResult()
+        {
+            LaunchProcess();
+            return RedirectToAction("Index");
+        }
+
+        private void LaunchProcess()
+        {
+            try
+            {
+                Process p = new Process();
+                p.StartInfo.FileName = configuration["ProcessInfo:ProcessFilePath"];
+                if (p.Start())
+                    ViewData[noticeName] = $"Process {p.ProcessName} has been started,uid as {p.Id}";
+                else
+                    ViewData[noticeName] = $"Fail to launch process";
+            }
+            catch (Exception ex)
+            {
+                ViewData[noticeName] = "Launching process failed with message " + ex.Message;
+            }
+        }
+
+        public ActionResult KillProcessResult(int uid)
+        {
+            KillProcess(uid);
+            return RedirectToAction("Index");
+        }
+        private void KillProcess(int uid)
+        {
+            try
+            {
+                Process tokill = Process.GetProcessById(uid);
+                if (tokill == null || tokill.HasExited)
+                {
+                    ViewData[noticeName] = "Process is already killed";
+                    return;
+                }
+                if (tokill.CloseMainWindow())
+                {
+                    if (tokill.WaitForExit(3000))
+                    {
+                        ViewData[noticeName] = $"Process {uid} has been closed";
+                        return;
+                    }
+                }
+
+                ForceKill(tokill);
+            }
+            catch (Exception ex)
+            {
+                ViewData[noticeName] = "Fail to kill process with message" + ex.Message;
+            }
+
+
+        }
+
+        private void ForceKill(Process tokill)
+        {
+            tokill.Kill();
+            if (tokill.WaitForExit(3000))
+                ViewData[noticeName] = $"Process {tokill.Id} has been killed";
+            else
+                ViewData[noticeName] = $"Fail to kill process {tokill.Id} , time out expired in 3 seconds";
         }
     }
 }
